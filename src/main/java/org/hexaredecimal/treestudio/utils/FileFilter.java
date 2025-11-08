90094ba70f7f358a038fee4b1868d699a6f32fb4
hexaredecimal <mfanakagama@gmail.com>
2025-11-09 00:31:53 +0200

    FileFilters: The last filter

---
+++ src/main/java/org/hexaredecimal/treestudio/utils/FileFilter.java
@@ -0,0 +1,13 @@
+package org.hexaredecimal.treestudio.utils;
+
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class FileFilter {
+		public static final FileNameExtensionFilter pngFilter = new FileNameExtensionFilter("(PNG) Portable Network Graphics", "png");
+		public static final FileNameExtensionFilter gifFilter = new FileNameExtensionFilter("Graphics Interchange Format", "gif");
+		public static final FileNameExtensionFilter treeFilter = new FileNameExtensionFilter("TreeStudio Project File", "tree");
+}
