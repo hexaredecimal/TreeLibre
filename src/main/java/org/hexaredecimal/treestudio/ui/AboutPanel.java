16b71017ee8b2cee58338625faeea43c9706e768
hexaredecimal <mfanakagama@gmail.com>
2025-11-09 22:53:31 +0200

    AboutPanel: Removed the netbeans template

--- src/main/java/org/hexaredecimal/treestudio/ui/AboutPanel.java
+++ src/main/java/org/hexaredecimal/treestudio/ui/AboutPanel.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
- */
 package org.hexaredecimal.treestudio.ui;
 
 import java.awt.event.ActionListener;
