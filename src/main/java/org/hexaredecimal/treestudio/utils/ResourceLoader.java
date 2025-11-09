290c40451f90cb8026d7370d66ba999c525e8a8d
hexaredecimal <mfanakagama@gmail.com>
2025-11-09 22:54:48 +0200

    ResourceLoader: Improved resource loader

--- src/main/java/org/hexaredecimal/treestudio/utils/ResourceLoader.java
+++ src/main/java/org/hexaredecimal/treestudio/utils/ResourceLoader.java
@@ -1,6 +1,9 @@
 package org.hexaredecimal.treestudio.utils;
 
 import java.awt.Image;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
 import javax.swing.ImageIcon;
 
 /**
@@ -19,4 +22,29 @@ public class ResourceLoader {
 		var icon = new ImageIcon(icn);
 		return icon.getImage();
 	}
+
+	public static File loadFileResource(String name) {
+		var path = String.format("/%s", name);
+		URL uri = ResourceLoader.class.getResource(path);
+		if (uri == null) {
+			throw new RuntimeException("Cannot load internal resource");
+		}
+
+		try {
+			return new File(uri.toURI());
+		} catch (URISyntaxException ex) {
+			throw new RuntimeException(ex.getMessage());
+		}
+	}
+
+	public static URL loadUrlResource(String name) {
+		var path = String.format("/%s", name);
+		URL uri = ResourceLoader.class.getResource(path);
+		if (uri == null) {
+			throw new RuntimeException("Cannot load internal resource");
+		}
+		return uri;
+	}
+
+	
 }
